7bb80a15d4a91e152ae2561735e00a5039069ad5
NeAleks18 <nealeks18@mail.ru>
2024-07-08 01:20:18 +0900

    perf - ???

--- Entities/Camera.cs
+++ Entities/Camera.cs
@@ -8,18 +8,17 @@ namespace unkwn.Entities;
 public class Camera : Cam3D
 {
     public Camera(Vector3 position, Vector3 target, Vector3 up, float fov = 90, CameraProjection projection = CameraProjection.Perspective, CameraMode mode = CameraMode.Free)
-        : base(position, target, up, fov, projection, mode) { }
+        : base(position, target, up, fov, projection, mode)
+    {
+        if (Mode == CameraMode.Free)
+        {
+            MouseSensitivity = 0.1f;
+        }
+    }
 
     protected override void Init()
     {
         base.Init();
-
-        if (this.Mode == CameraMode.Free)
-        {
-            Input.HideCursor();
-            Input.DisableCursor();
-            this.MouseSensitivity = 0.1f;
-        }
     }
 
     protected override void FixedUpdate()
@@ -27,8 +26,21 @@ public class Camera : Cam3D
         base.FixedUpdate();
     }
 
+
     protected override void Update()
     {
         base.Update();
+
+        if (Mode != CameraMode.Free && Input.IsMouseButtonDown(MouseButton.Right))
+        {
+            Mode = CameraMode.Free;
+            Input.HideCursor();
+            Input.DisableCursor();
+        } else if (Mode != CameraMode.Custom && !Input.IsMouseButtonDown(MouseButton.Right))
+        {
+            Mode = CameraMode.Custom;
+            Input.ShowCursor();
+            Input.EnableCursor();
+        }
     }
 }
--- Scenes/MainScene.cs
+++ Scenes/MainScene.cs
@@ -20,7 +20,7 @@ public class MainScene : Scene
         base.Init();
 
         Vector3 pos = new Vector3(10.0f, 10.0f, 10.0f);
-        Camera cam3D = new Camera(pos, Vector3.Zero, Vector3.UnitY, 90, CameraProjection.Perspective, CameraMode.Free);
+        Camera cam3D = new Camera(pos, Vector3.Zero, Vector3.UnitY);
         this.AddEntity(cam3D);
 
         TestEntity testEntity = new TestEntity(new Vector3(0f,0f,0f));
